03c24b1423383e2aa4eee840507e14d68fe60864
jonathantan123 <jonathantan@brandeis.edu>
2020-03-10 14:15:33 -0400

    updates

--- backend/app/controllers/api/v1/transactions_controller.rb
+++ backend/app/controllers/api/v1/transactions_controller.rb
@@ -12,6 +12,7 @@ class Api::V1::TransactionsController < ApplicationController
         ## first find the stock  
     
         user = User.find(params[:user_id])
+     
         
         query = params[:ticker].capitalize() 
         url = "https://cloud.iexapis.com/stable/stock/#{query}/quote?token=#{Rails.application.credentials[:apiKey]}&filter=companyName,symbol,latestPrice,previousClose"
@@ -19,12 +20,29 @@ class Api::V1::TransactionsController < ApplicationController
 
         if (response.body === "Unknown symbol") 
             render json: {error: "Invalid Ticker"}
-        else
+        end 
+
             json = JSON.parse response.body
-            newTransaction = user.transactions.create(ticker: "#{json["symbol"]}", quantity: params[:quantity].to_i, price: "#{json["latestPrice"]}", type: "BUY")
-         
-    
-        end
+            
+            userBalance = user.balance.to_f.round(2)
+            totalCost = (json["latestPrice"].to_f().round(2) * params[:quantity].to_i())
+            
+
+
+            if(totalCost > userBalance )  
+                
+                render json: {error: "you dont have enough funds "}
+                puts "nope"
+         else 
+                    
+                    
+                  user.update(balance: user.balance - totalCost )               
+                    newTransaction = user.transactions.create(ticker: "#{json["symbol"]}", quantity: params[:quantity].to_i, price: "#{json["latestPrice"]}")
+
+                    render json: {message: "Purchased!", balance: user.balance }
+
+                
+            end
     end  
 
 end 
