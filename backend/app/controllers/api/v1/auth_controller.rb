23b3ebe9087f6b353918f8899f70ad3c5273663e
jonathantan123 <jonathantan@brandeis.edu>
2020-03-12 11:03:46 -0400

    adding files to gitignore

---
+++ backend/app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,19 @@
+ class Api::V1::AuthController < ApplicationController
+
+
+    def login 
+        
+        user = User.find_by(email_address: params[:email_address])
+        
+        if user && user.authenticate(params[:password])
+            
+            render json:  user
+        else 
+            render json:{errors: "invalid username/ password"}
+            
+        end 
+
+    end 
+
+
+end
