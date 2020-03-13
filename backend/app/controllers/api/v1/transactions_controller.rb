28849f9c0368bd376d4af8fb15c9d061ac127e23
jonathantan123 <jonathantan@brandeis.edu>
2020-03-13 17:22:27 -0400

    adding comments

--- backend/app/controllers/api/v1/transactions_controller.rb
+++ backend/app/controllers/api/v1/transactions_controller.rb
@@ -1,66 +1,54 @@
 class Api::V1::TransactionsController < ApplicationController
 
-
-    def index 
-        trans = Transaction.all
-        render json: trans
-
-    end 
-
-
     def create 
-        ## first find the stock  
-    
+        ### 1) find the user through params sent from front end 
         user = User.find(params[:user_id])
      
-        
-        query = params[:ticker].capitalize() 
+        #### 2) Set query term to ticker sent from front (query is not case sensitive)
+        query = params[:ticker]
+
+        ### 3) Url filtered just to get necessary json information, no need to return all 
         url = "https://cloud.iexapis.com/stable/stock/#{query}/quote?token=#{Rails.application.credentials[:apiKey]}&filter=companyName,symbol,latestPrice,previousClose"
         response = HTTParty.get("#{url}")
 
-        
+        ### 4) check response json. Api will return unknown symbol if a ticker does not exist. 
 
         if (response.body === "Unknown symbol") 
             render json: {error: "Invalid Ticker"}
         else 
-
+            ### 5) Else parse json. Find user balance and calculate total cost of the transaction.
             json = JSON.parse response.body
             userBalance = user.balance.to_f.round(2)
             ticker = json["symbol"]
             latestPrice = json["latestPrice"].to_f().round(2)
             totalCost = latestPrice * params[:quantity].to_i()
-            
-   
-            
+            ### if User does not have enough funds, render error 
             if(totalCost > userBalance )  
                 render json: {error: "you dont have enough funds "}
             else   
+                ### Else find the user portfolio and thier stocks saved in an array 
                 portfolio = user.portfolio
                 portfolioStocks = portfolio.stocks
-                
-          
+                ### go through all of the stocks in the portfolio to see if the ticker already exists 
                 foundStock = portfolioStocks.find{ |stock| stock.ticker === ticker} 
-
+                ### if it exists, increase the quantity by qty user inputted
                 if (foundStock)
                     foundStock.quantity += params[:quantity].to_i()
-                    foundStock.price = latestPrice
+                    # foundStock.price = latestPrice
                  else  
-                    newStock = Stock.create(ticker: "#{json["symbol"]}", price: "#{json["latestPrice"]}", quantity: params[:quantity].to_i(), color: "gray")
+                    ### otherwise, create a new stock and add it to the user's portfolio of stocks
+                    newStock = Stock.new(ticker: "#{json["symbol"]}", price: "#{json["latestPrice"]}", quantity: params[:quantity].to_i(), color: "gray")
                     portfolioStocks.push(newStock)
                  end 
+                    ### Update user's portfolio and balance. Create a new transaction and send back to front end. 
 
+                    
                   portfolio.update(stocks: portfolioStocks)
-            
                   user.update(balance: user.balance - totalCost )
-               
                   newTransaction = user.transactions.create(ticker: "#{json["symbol"]}", quantity: params[:quantity].to_i, price: "#{json["latestPrice"]}")
-
                  render json: {message: "Purchased!", balance: user.balance }
-
-                
             end
-    end  
-
-end 
+        end  
+    end 
 end 
 
--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,40 +1,25 @@
 class Api::V1::UsersController < ApplicationController
 
-    
-    # def index 
-    #     users = User.all
-    #     render json: users
-        
-    # end 
-
-
     def show 
-        user = User.find(params[:id])
-        
+        user = User.find(params[:id]) 
         transactions = user.transactions
         portfolioStocks = user.portfolio.stocks
 
         render json: {transactions: transactions}
     end 
 
-
     def create 
-        
         user = User.new(
             email_address: params[:email_address],
             password: params[:password], 
             name: params[:name], 
             )
-        
         if user.save 
             Portfolio.create(user_id: user.id)
             render json: user 
         else 
             render json: {errors: user.errors.full_messages}
-        
+        end 
     end 
-end 
-
-
 
 end 
