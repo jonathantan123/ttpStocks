0189d786d72e33b8ddb5f267d235eb68b489f8bf
jonathantan123 <jonathantan@brandeis.edu>
2020-03-13 17:52:04 -0400

    adding more comments

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,23 @@
-# ttpStock
+
+
+## Noodums 
+________________
+
+A Stock Portofilio App built to track share performance deployed to heroku: 
+https://ttp-stocker-app.herokuapp.com/
+
+
+### `Tech`
+
+This app is built with a Rails Backend and a React frontend using Semantic Ui for styling. 
+
+App is connected to the IEX Cloud API in order to fetch real time share data.
+
+
+
+### ` To Use `
+1) Register a new user account 
+2) Naviagate to the portfolio page and input ticker and quantity of stock to purchase
+
+
+
--- backend/app/controllers/find_controller.rb
+++ backend/app/controllers/find_controller.rb
@@ -1,31 +1,21 @@
 class FindController < ApplicationController
 
-
-
-
     def find 
-
+    ### 1) Find user portfolio and corresponding stocks
        portfolio =  Portfolio.find_by(user_id: params[:id])
        portfolioStocks = portfolio.stocks 
-
+    ### 2) Map through all stocks and fetch to API for each individual stock
        portfolioStocks.map do |stock| 
-    
-        url = "https://cloud.iexapis.com/stable/stock/#{stock.ticker}/quote?token=#{Rails.application.credentials[:apiKey]}&filter=open,close,latestPrice,previousClose,iexRealtimePrice"
-
-        response = HTTParty.get("#{url}")
-        
-
-        json = JSON.parse response.body
-        
-        
-        
-         stock.price = json['latestPrice'].to_f().round(2)
-         stock.total = stock.price * stock.quantity
-       
-            
-           comparisionPrice =  json["open"] ? "open" : "previousClose"
-           
-
+            url = "https://cloud.iexapis.com/stable/stock/#{stock.ticker}/quote?token=#{Rails.application.credentials[:apiKey]}&filter=open,close,latestPrice,previousClose,iexRealtimePrice"
+            response = HTTParty.get("#{url}")
+            json = JSON.parse response.body
+            ### 3) Update stock price and total with latest data from API
+            stock.price = json['latestPrice'].to_f().round(2)
+            stock.total = stock.price * stock.quantity
+            ### 4) if stock market has closed, then "open" attribute will be null. In this case 
+            ###    use the previous close price to compare against current latest price. 
+            comparisionPrice =  json["open"] ? "open" : "previousClose"
+            ### 5) Check stock price against comparision price to set what price should display on front end
             if (stock.price > json[comparisionPrice].to_f()) 
                 stock.color = "green"
             elsif (json["latestPrice"].to_f() < json[comparisionPrice].to_f())
@@ -33,17 +23,8 @@ class FindController < ApplicationController
             else 
                 stock.color = "grey" 
             end 
-
-
         end 
-
-
         render json: portfolioStocks
-        
-       
-
     end 
-
-
 end
  
